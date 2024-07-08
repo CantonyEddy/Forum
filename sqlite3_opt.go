539764a1616b35f4633111ee59af88d837efe077
RYUJINC <cantony.eddy@gmail.com>
2024-07-08 16:56:11 +0200

    commit sql3 https

--- sqlite3_opt.go
+++ sqlite3_opt.go
@@ -1,1 +1,11 @@
-package main
+//go:build sqlite_see
+// +build sqlite_see
+
+package sqlite3
+
+/*
+#cgo CFLAGS: -DSQLITE_HAS_CODEC
+#cgo LDFLAGS: -lcrypto
+#cgo LDFLAGS: -lsqlcipher
+*/
+import "C"
